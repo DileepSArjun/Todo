1617b4c9c10a19fcc9a2da32b0ea597c2c2a5c91
DSA <dileep@qburst.com>
2014-07-02 20:03:27 +0530

    added control event binding

--- js/view_handler.js
+++ js/view_handler.js
@@ -36,7 +36,7 @@ var TodosList = function(settings){
 		bo.innerHTML = "<div class='view'>"+
 						"<input class='toggle' type='checkbox'"+(item.done ? 'checked' : '')+">"+
 						"<label title='"+item.desc+"'>"+item.title+"</label>"+
-						"<span class='priority span-label' title='Priority'>"+item.priority+"</span>"+
+						"<span class='priority span-label p"+item.priority+"' title='Priority'>"+item.priority+"</span>"+
 						"<span class='order span-label' title='Order'>"+item.order+"</span>"+
 						"<a class='destroy'></a>"+
 					"</div>";
@@ -46,24 +46,57 @@ var TodosList = function(settings){
 
 	function init(){
 		that.reload();
+		that._bindControls();
 	};
 
 
 	this._nextOrder = 1;
+	this._itemsLeft = 0;
+	this._isEdit = false;
+
+	this._modifItemsLeft = function(){
+		var itemsBo = $$.findAll('li', options.holder),
+			itemsCompleted = 0;;
+
+		that._itemsLeft = 0;
+		for(var i = 0; i < itemsBo.length; i++){
+			if(!$$.hasClass(itemsBo[i], 'task-completed')){
+				that._itemsLeft++;
+			}else {
+				itemsCompleted++;
+			}
+		}
+
+		if(options.controls){
+			options.controls.itemsLeft && (options.controls.itemsLeft.innerHTML = that._itemsLeft + " items left");
+			options.controls.clearFinished && (options.controls.clearFinished.innerHTML = "Clear "+itemsCompleted+" completed item's");
+		}
+	};
 
 	this._bindEventsOnList = function(bo){
 		bo.addEventListener('click', function(event){
 
 			if(event.target.className === "toggle"){
 				$$.toggleClass(event.currentTarget, 'task-completed');
+				that._modifItemsLeft();
 			}else if(event.target.className === "destroy"){
-				options.holder.removeChild(event.currentTarget);
+				that.remove(event.currentTarget);
 			}else {
 				options.detailView && that._populateDetails(JSON.parse(event.currentTarget.getAttribute('data-attr')));
 			}
 		});
 
 		return bo;
+	};
+
+	this._bindControls = function(){
+		if(!options.controls) return false;
+
+		options.controls.clearFinished && options.controls.clearFinished.addEventListener('click', function(){
+			that.clearCompleted();
+		});
+
+		
 	};
 
 	this._populateDetails = function(details){
@@ -78,10 +111,14 @@ var TodosList = function(settings){
 	this.add = function(item){
 		options.holder.appendChild(that._bindEventsOnList(addItem(item)));
 		that._nextOrder++;
+		!item.done && that._modifItemsLeft();
 	};
 
-	this.remove = function(){
-		return true;
+	this.remove = function(item){
+		var id = item.id;
+		options.holder.removeChild(item);
+		that._modifItemsLeft();
+		//add call to remove the item from local-satorage with the same id
 	};
 
 	this.edit = function(){};
@@ -97,6 +134,16 @@ var TodosList = function(settings){
 
 		for(var i = 0; i < dataSet.length; i++){
 			that.add(dataSet[i]);
+		}
+	};
+
+	this.clearCompleted = function(){
+		var itemsBo = $$.findAll('li', options.holder);
+
+		for(var i = 0; i < itemsBo.length; i++){
+			if($$.hasClass(itemsBo[i], 'task-completed')){
+				that.remove(itemsBo[i]);
+			}
 		}
 	};
 
